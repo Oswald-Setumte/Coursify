1af597baa7419deeb3d9fbdf6abbce91aece23b8
Oswald-Setumte <baninioswaldsetumte@gmail.com>
2025-11-10 03:27:15 -0800

    create the Status bar for the dashboard

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -3,6 +3,17 @@ import React from 'react'
 export default function Dashboard() {
   return (
     <div>
+      {/* main */}
+      <div>
+        {/* Status bars */}
+        <div>
+
+        </div>
+
+
+      </div>
+      {/* Calendar Section */}
+      <div></div>
 
     </div>
   )
